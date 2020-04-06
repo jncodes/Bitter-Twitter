267d42f0c53a6a17034d32d47a5736d995728b9f
jncodes <midn.nelson@gmail.com>
2020-04-06 01:27:52 -0400

    Add user registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login')
+
 router.get('/test', (req, res) => res.json({
     msg: 'This is the users route'
 }));
+
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                })
+            }
+        })
+})
 
 module.exports = router;
